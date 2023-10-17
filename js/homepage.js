6640a5434137ba265b65ac6f663bf22eafde1fc5
Abhinav Saxena <abhinav150601@gmail.com>
2023-10-17 14:32:06 +0530

    changed UI

--- js/homepage.js
+++ js/homepage.js
@@ -5,17 +5,14 @@ function fetchWeatherData() {
     const rainPercentage = document.querySelector('.rain-percentage');
     const temperature = document.querySelector('.temperature');
     const weatherIcon = document.querySelector('.weather-icon');
+    
 
-    // Fetch weather data
     fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchInput.value}`)
       .then(response => response.json())
       .then(data => {
-        // Update the HTML elements with the weather data
         city.textContent = data.location.name;
         rainPercentage.textContent = `Chances Of Rain: ${data.current.precip_in}%`;
         temperature.textContent = `${data.current.temp_c}°`;
-
-        // Assuming the weather icon is provided in the API response
         weatherIcon.src = data.current.condition.icon;
       })
       .catch(error => console.error('Error:', error));
@@ -23,11 +20,6 @@ function fetchWeatherData() {
       if (searchInput.value == "") {
         alert("Please enter a city");
       }
-
-      // Clear the input field
-      // searchInput.value = '';
-
-      //if not a valid city
       if (data.location.name == "") {
         alert("Please enter a valid city");
       }
@@ -37,9 +29,4 @@ function fetchWeatherData() {
     document.querySelector('.weather-icon img').src = weatherData.weatherIcon;
   } else {
     document.querySelector('.weather-icon img').style.visibility = 'hidden';
-  }
-
-
-    // Listen for the form submit event, then call the fetchWeatherData function
-// document.querySelector('.search-container').addEventListener('change', fetchWeatherData);
-
+}
